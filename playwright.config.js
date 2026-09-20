938f4f36b49ec72a2af8fcb5da51e97cda2b15fd
hns140412-glitch <hns140412@gmail.com>
2026-09-21 07:25:27 +0900

    test: isolate Hide E2E from service-worker reloads

--- playwright.config.js
+++ playwright.config.js
@@ -5,7 +5,8 @@ module.exports=defineConfig({
   timeout:30000,
   use:{
     baseURL:'http://127.0.0.1:4174',
-    headless:true
+    headless:true,
+    serviceWorkers:'block'
   },
   webServer:{
     command:'python3 -m http.server 4174',
