2967540a4df9cc2bd69ef219cdb8498886eef176
hns140412-glitch <hns140412@gmail.com>
2026-09-21 12:41:10 +0900

    chore: cache verified language evidence asset

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
-const CACHE="hide-seek-capture-v11";
+const CACHE="hide-seek-capture-v12";
 const CORE=[
   "./","./index.html","./styles.css","./hide-runtime.css","./hide-bridge.css","./app.js","./hide-family-ocr-adapter.js",
-  "./hide-language-model.js","./hide-runtime.js","./hide-bridge.js","./hide-brand-current.js","./manifest.json","./assets/asset-map.json",
+  "./hide-language-evidence.js","./hide-language-model.js","./hide-runtime.js","./hide-bridge.js","./hide-brand-current.js","./manifest.json","./assets/asset-map.json",
   "./assets/icons/icon-180x180.png","./assets/icons/icon-192x192.png","./assets/icons/icon-512x512.png",
   "./assets/backgrounds/academy.png","./assets/backgrounds/park.png","./assets/backgrounds/bookstore.png","./assets/backgrounds/cafe.png","./assets/backgrounds/classroom.png","./assets/backgrounds/station.png",
   "./assets/characters/guide_default.png","./assets/characters/guide_smile.png","./assets/characters/guide_hint.png","./assets/characters/guide_note.png","./assets/characters/guide_radio.png","./assets/characters/guide_fever.png","./assets/characters/guide_focus.png","./assets/characters/guide_cheer.png","./assets/characters/rabbit.png","./assets/characters/fennec.png","./assets/characters/sloth.png",
