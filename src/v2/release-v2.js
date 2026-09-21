07236561929605ba670a7a6c582cb23f523cf827
hns140412-glitch <hns140412@gmail.com>
2026-09-21 20:40:50 +0900

    rewrite: add V2 release descriptor

---
+++ src/v2/release-v2.js
@@ -0,0 +1,13 @@
+(function(root){
+  'use strict';
+  root.HideSeekV2ReleaseDescriptor=Object.freeze({
+    app_id:'hide-seek-v2',
+    app_version:'V2_PREVIEW_01',
+    runtime_version:'2026.09.21-v2',
+    data_schema_version:1,
+    contract_version:1,
+    release_id:'hide-seek-v2-preview-01',
+    master_revision:'RUNTIME_V2_REWRITE',
+    shared_runtime_basis:'TAKY@PRODUCT-INTEGRITY'
+  });
+})(typeof globalThis!=='undefined'?globalThis:this);
